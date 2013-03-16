3567725a4d858abff046239d1a7d8ad4b763ca85
Matthew Conger-Eldeen <mceldeen@gmail.com>
2013-03-15 20:30:00 -0600

    Add license statement to all source code

--- lib/Mce/Date/Range/LeftInclusive.php
+++ lib/Mce/Date/Range/LeftInclusive.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of the Mce package.
+ *
+ * (c) Matthew Conger-Eldeen <mceldeen@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Mce\Date\Range;
 
 /**
--- tests/Mce/File/Parse/IniTest.php
+++ tests/Mce/File/Parse/IniTest.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of the Mce package.
+ *
+ * (c) Matthew Conger-Eldeen <mceldeen@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Mce\File\Parse;
 
 require_once dirname( __FILE__ ) . '/../../../../lib/Mce/File/Parse/Ini.php';
