1a7d1ce6ded3402d1e395b04a2a839e7b78c51df
Matthew Conger-Eldeen <mceldeen@gmail.com>
2013-08-06 13:22:15 -0600

    Don't force timezones to be the same

--- lib/Mce/Date/RangeAbstract.php
+++ lib/Mce/Date/RangeAbstract.php
@@ -18,23 +18,22 @@ abstract class RangeAbstract implements RangeInterface {
     protected $start  = null;
     protected $end    = null;
 
-    public function __construct(\DateTime $start, \DateTime $end) { 
+    public function __construct(\DateTime $start, \DateTime $end) {
         if($start >= $end) {
             throw new \LogicException("The start of your range must be before the end of your range.");
         }
-        
+
         $this->start = $start;
         $this->end = $end;
-        $this->end->setTimezone($start->getTimezone());
     }
-    
+
     /**
      * @return \DateTime the start of the range
      */
     public function getStart() {
         return $this->start;
     }
-    
+
     /**
      * @return \DateTime the end of the range
      */
